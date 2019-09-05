1e45a666f87e89a6242d144fd38b0f5f108126cf
abidex4yemi <abidex4yemi@gmail.com>
2019-09-05 15:59:46 +0100

    Write basic react test for app component

---
+++ react-testing-1/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <span className="greeting">Welcome</span>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-testing-1/src/App.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import * as rtl from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
+import App from './App';
+
+describe('App Test suite', () => {
+  it('Should render a span with text (Welcome)', () => {
+    const wrapper = rtl.render(<App />);
+    // console.log(wrapper.debug());
+    const element = wrapper.queryByText(/Welcome/);
+    expect(element).toBeInTheDocument();
+    expect(element).toBeTruthy();
+    expect(element).toBeVisible();
+  });
+});
